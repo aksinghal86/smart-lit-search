c86ea2f9ac98749cfc9696ec22cd2ab6c85ddc10
Ankur Singhal <asinghal@EHE.local>
2022-08-30 10:46:54 -0700

    no pacman

--- dashboard/global.R
+++ dashboard/global.R
@@ -1,22 +1,16 @@
-if (!require(pacman)) install.packages('pacman')
+# if (!require(pacman)) install.packages('pacman')
 # if (!require(particlesjs)) devtools::install_github("dreamRs/particlesjs")
 
-pacman::p_load(
-  # Shiny and related UI features
-  shiny,  
-  waiter, # for loading screen
-  tippy, # for tooltips
-  # promises, # ASYNC
-  # future, # ASYNC
-  easyPubMed, # Pubmed search 
-  tidyverse, 
-  tidytext, # for text processing
-  tm, # for text processing, 
-  ggiraph, # for interactive graphs
-  wordcloud
-  # ggforce
-  # reactable # for interactive table
-)
+library(shiny)
+library(waiter)
+library(tippy)
+library(easyPubMed) 
+library(tidyverse)
+library(tidytext)
+library(tm)
+library(ggiraph)
+library(wordcloud)
+library(particlesjs)
 
 source('functions.R')
 api_key <- read_lines('assets/ncbi-key.txt')
